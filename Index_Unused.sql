9765c0cfe0f241172980341c7563835d7ee1a4d9
Subbu CN <111134712+AzureSQLChamp@users.noreply.github.com>
2023-02-14 15:45:14 +0530

    Create Index_Unused.sql

---
+++ Index_Unused.sql
@@ -0,0 +1,35 @@
+--Script #– TOTALLY UNUSED INDEXES
+SELECT TOP 25
+o.name AS ObjectName
+, i.name AS IndexName
+, i.index_id AS IndexID
+, dm_ius.user_seeks AS UserSeek
+, dm_ius.user_scans AS UserScans
+, dm_ius.user_lookups AS UserLookups
+, dm_ius.user_updates AS UserUpdates
+, p.TableRows
+, 'DROP INDEX ' + QUOTENAME(i.name)
++ ' ON ' + QUOTENAME(s.name) + '.'
++ QUOTENAME(OBJECT_NAME(dm_ius.OBJECT_ID)) AS 'drop statement'
+FROM sys.dm_db_index_usage_stats dm_ius
+INNER JOIN sys.indexes i ON i.index_id = dm_ius.index_id 
+AND dm_ius.OBJECT_ID = i.OBJECT_ID
+INNER JOIN sys.objects o ON dm_ius.OBJECT_ID = o.OBJECT_ID
+INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+INNER JOIN (SELECT SUM(p.rows) TableRows, p.index_id, p.OBJECT_ID
+FROM sys.partitions p GROUP BY p.index_id, p.OBJECT_ID) p
+ON p.index_id = dm_ius.index_id AND dm_ius.OBJECT_ID = p.OBJECT_ID
+WHERE OBJECTPROPERTY(dm_ius.OBJECT_ID,'IsUserTable') = 1
+AND dm_ius.database_id = DB_ID()
+AND i.type_desc = 'nonclustered'
+AND i.is_primary_key = 0
+AND i.is_unique_constraint = 0
+ORDER BY (dm_ius.user_seeks + dm_ius.user_scans + dm_ius.user_lookups) ASC
+GO
+
+--/*Note:
+User Seeks indicates how many times an index seek occurred for a particular index.  User Seeks are the ideal way to access data as they are the fastest as less resource intensive option.  You generally want to see a higher value of Seeks than Scans or Lookups.
+A User Scan occurs when multiple rows of data are searched in order to locate the data being pulled back by the query.  This is less ideal than a user seek and you want to try to avoid these if possible.
+User Lookups indicate the number of times a query had to pull the data from a clustered index or from the heap.
+User Updates indicate the number of times the index was updated due to changes within the data.*/
+
