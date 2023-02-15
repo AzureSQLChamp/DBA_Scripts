79b5c0536ba4866ace4114376ecd41b43a978853
Subbu CN <111134712+AzureSQLChamp@users.noreply.github.com>
2023-02-15 14:30:36 +0530

    Create Very-Large-Databases-Optimization.sql

---
+++ Very-Large-Databases-Optimization.sql
@@ -0,0 +1,4 @@
+https://blogs.msdn.microsoft.com/sqlserverstorageengine/2007/04/30/quick-list-of-vldb-maintenance-best-practices/
+http://shaunjstuart.com/archive/2013/11/monitoring-for-endless-index-defragmenting/
+https://www.youtube.com/watch?v=cN30SRgPkTk&list=PLFUGPe1byxev9kXxmCXvYrLXJ5gxP5JAT&index=49&t=10s
+https://www.sqlshack.com/ms-sql-server-backup-optimization/
