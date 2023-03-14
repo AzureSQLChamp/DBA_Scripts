46b8fff06ad906da69e07bd360cbcea235d36070
Subbu CN <111134712+AzureSQLChamp@users.noreply.github.com>
2023-03-14 12:31:59 +0530

    Update CPU#-High-CPU-usage issues in SQL Server.sql

--- CPU#-High-CPU-usage issues in SQL Server.sql
+++ CPU#-High-CPU-usage issues in SQL Server.sql
@@ -27,6 +27,27 @@ Get-Counter -Counter $Counters -MaxSamples 30 | ForEach {
 }
 
 /*
+Database Level CPU utilization
+*/
+
+WITH DB_CPU AS
+(SELECT	DatabaseID, 
+		DB_Name(DatabaseID)AS [DatabaseName], 
+		SUM(total_worker_time)AS [CPU_Time(Ms)] 
+FROM	sys.dm_exec_query_stats AS qs 
+CROSS APPLY(SELECT	CONVERT(int, value)AS [DatabaseID]  
+			FROM	sys.dm_exec_plan_attributes(qs.plan_handle)  
+			WHERE	attribute =N'dbid')AS epa GROUP BY DatabaseID) 
+SELECT	ROW_NUMBER()OVER(ORDER BY [CPU_Time(Ms)] DESC)AS [SNO], 
+	DatabaseName AS [DBName], [CPU_Time(Ms)], 
+	CAST([CPU_Time(Ms)] * 1.0 /SUM([CPU_Time(Ms)]) OVER()* 100.0 AS DECIMAL(5, 2))AS [CPUPercent] 
+FROM	DB_CPU 
+WHERE	DatabaseID > 4 -- system databases 
+	AND DatabaseID <> 32767 -- ResourceDB 
+ORDER BY SNO OPTION(RECOMPILE); 
+
+
+/*
 To identify the queries that are responsible for high-CPU activity currently, run the following statement:
 */
 SELECT TOP 10 s.session_id,
