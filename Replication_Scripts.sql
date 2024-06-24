92b1e1c28be2bf1f3c01a3753cb5772ec97c3da0
Subbu CN <111134712+AzureSQLChamp@users.noreply.github.com>
2024-06-24 12:12:28 +0530

    Update Replication_Scripts.sql

--- Replication_Scripts.sql
+++ Replication_Scripts.sql
@@ -1,4 +1,47 @@
+--Replication features is_installed status check on the instance
 
+DECLARE @replication_installed int;  
+EXEC @replication_installed = master.sys.sp_MS_replication_installed;  
+SELECT @replication_installed as Is_Installed;
+
+--check Database replication enabled status  
+select @@servername
+select name, is_published, is_subscribed, is_distributor
+  from sys.databases
+  where is_published = 1 or is_subscribed = 1 or is_distributor = 1
+
+--Replication details
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED 
+
+IF EXISTS (SELECT 1 FROM master..sysdatabases WHERE name = 'Distribution')
+BEGIN
+	-- Get the publication name based on article 
+	SELECT DISTINCT  
+		 p.publication								AS Publication_Name
+		,srv.srvname								AS Publication_Server  
+		,a.publisher_db								AS Publication_Database
+		,a.article									AS Publication_Table_Name
+		,ss.srvname									AS Subscription_Server  
+		,s.subscriber_db							AS Subscription_Database
+		,a.destination_object 						AS Subscription_Table_Name
+		,da.subscriber_login				 		AS Subscription_Login
+		,da.name							 		AS Distribution_Agent_Job_Name
+	FROM Distribution..MSArticles a  
+	JOIN Distribution..MSpublications p 
+		ON a.publication_id = p.publication_id 
+	JOIN Distribution..MSsubscriptions s 
+		ON p.publication_id = s.publication_id 
+	JOIN master..sysservers ss 
+		ON s.subscriber_id = ss.srvid 
+	JOIN master..sysservers srv 
+		ON srv.srvid = p.publisher_id 
+	JOIN Distribution..MSdistribution_agents da 
+		ON da.publisher_id = p.publisher_id  
+		AND da.subscriber_id = s.subscriber_id 
+	ORDER BY 1,2,3 
+END
+
+===========================
 SELECT 
   msp.publication AS PublicationName,
   msa.publisher_db AS DatabaseName,
