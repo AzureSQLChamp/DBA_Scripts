9912cc10c753d7187b8edbb702c4efc57f24133f
Subbu CN <111134712+AzureSQLChamp@users.noreply.github.com>
2024-08-20 11:25:07 +0530

    Update CPU#-High-CPU-usage issues in SQL Server.sql

--- CPU#-High-CPU-usage issues in SQL Server.sql
+++ CPU#-High-CPU-usage issues in SQL Server.sql
@@ -90,6 +90,28 @@ FROM sys.dm_exec_query_stats qs
 CROSS APPLY sys.dm_exec_sql_text(sql_handle) st
 ORDER BY(qs.total_worker_time / qs.execution_count) DESC
 
+--SQL Server – Get CPU Usage History for last 2days
+	
+DECLARE​​ @ts_now​​ bigint​​ =​​ (SELECT​​ cpu_ticks/(cpu_ticks/ms_ticks)​​ FROM​​ sys.dm_os_sys_info​​ WITH​​ (NOLOCK));​​ 
+SELECT​​ SQLProcessUtilization​​ AS​​ [SQL Server Process CPU Utilization],​​ 
+ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​​​ SystemIdle​​ AS​​ [System Idle Process],​​ 
+  ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​​​ 100​​ -​​ SystemIdle​​ -​​ SQLProcessUtilization​​ AS​​ [Other Process CPU Utilization],​​ 
+   ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​​​​ DATEADD(ms,​​ -3 *​​ (@ts_now​​ -​​ [timestamp]),​​ GETDATE())​​ AS​​ [Event Time]​​ 
+FROM​​ (​​ 
+  ​​​​ SELECT​​ record.value('(./Record/@id)[1]',​​ 'int')​​ AS​​ record_id,​​ 
+   record.value('(./Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]',​​ 'int')​​ 
+   AS​​ [SystemIdle],​​ 
+   record.value('(./Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]',​​ 
+   'int')​​ 
+   AS​​ [SQLProcessUtilization],​​ [timestamp]​​ 
+  ​​​​ FROM​​ (​​ 
+   SELECT​​ [timestamp],​​ CONVERT(xml,​​ record)​​ AS​​ [record]​​ 
+   FROM​​ sys.dm_os_ring_buffers​​ WITH​​ (NOLOCK)
+   WHERE​​ ring_buffer_type​​ =​​ N'RING_BUFFER_SCHEDULER_MONITOR'​​ 
+   AND​​ record​​ LIKE​​ N'%<SystemHealth>%')​​ AS​​ x​​ 
+  ​​​​ )​​ AS​​ y​​ 
+ORDER​​ BY​​ record_id​​ DESC​​ OPTION​​ (RECOMPILE);
+
 Ref:
 https://learn.microsoft.com/en-us/troubleshoot/sql/database-engine/performance/troubleshoot-high-cpu-usage-issues
 
