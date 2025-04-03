d6fc0d425d8086c0b7846abf7650229e9f1ce582
Subbu CN <111134712+AzureSQLChamp@users.noreply.github.com>
2025-04-03 10:43:54 +0530

    Replication_Scripts.sql

--- Replication_Scripts.sql
+++ Replication_Scripts.sql
@@ -41,6 +41,45 @@ SELECT DISTINCT
 	ORDER BY 1,2,3 
 END
 
+--Replication Details along with Agent jobs info
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+IF EXISTS (SELECT 1 FROM master.dbo.sysdatabases WHERE name = 'distribution')
+BEGIN
+    PRINT 'Distribution database exists. Running the script...';
+    SELECT 
+        pub.publication AS Publication_Name,
+        pub.publisher_db AS Publication_Database,
+        p_srv.srvname AS Publisher_Server,  -- Corrected Publisher Server
+        art.source_owner + '.' + art.source_object AS Publication_Table_Name,
+        sub.subscriber_db AS Subscription_Database,
+        s_srv.srvname AS Subscription_Server,
+        ISNULL(art.destination_owner + '.' + art.destination_object, art.source_owner + '.' + art.source_object) AS Subscription_Table_Name,
+        sj.name AS Snapshot_Agent_Job_Name,
+        lj.name AS Log_Reader_Agent_Job_Name,
+        dj.name AS Distributor_Agent_Job_Name
+
+    FROM distribution.dbo.MSpublications pub
+    JOIN distribution.dbo.MSarticles art ON pub.publication_id = art.publication_id
+    JOIN distribution.dbo.MSsubscriptions sub ON pub.publication_id = sub.publication_id
+    JOIN master.dbo.sysservers p_srv ON pub.publisher_id = p_srv.srvid  
+    JOIN master.dbo.sysservers s_srv ON sub.subscriber_id = s_srv.srvid  
+    LEFT JOIN distribution.dbo.MSsnapshot_agents snap ON pub.publication = snap.publication
+    LEFT JOIN msdb.dbo.sysjobs sj ON snap.job_id = sj.job_id  
+    LEFT JOIN distribution.dbo.MSlogreader_agents logr ON pub.publisher_db = logr.publisher_db
+    LEFT JOIN msdb.dbo.sysjobs lj ON logr.job_id = lj.job_id  
+    LEFT JOIN distribution.dbo.MSdistribution_agents dist 
+        ON sub.subscriber_id = dist.subscriber_id 
+        AND sub.subscriber_db = dist.subscriber_db  
+    LEFT JOIN msdb.dbo.sysjobs dj ON dist.job_id = dj.job_id;  
+END
+ELSE
+BEGIN
+    PRINT 'Distribution database does not exist. Exiting script...';
+END;
+
+--Replication errors
+SELECT * FROM MSrepl_errors ORDER BY time desc
 
 --Change Replication Administrative (distributor_admin) login password
 USE master;
